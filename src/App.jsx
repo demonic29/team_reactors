4a29dafc82350c355aebe892314a10acc6701cd7
nam <>
2024-04-22 11:04:19 +0900

    boiderPlate

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div
+			style={{
+				display: "grid",
+				minHeight: "100vh",
+				placeItems: "center",
+			}}
+		>
+			Hello World!
+		</div>
+	);
+};
+
+export default App;
