cc1d5111bcaf70db7fea255c892cdfcff31b702b
UTakuto <taku.0307.ue@icloud.com>
2024-06-24 10:25:52 +0900

    update button

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -3,48 +3,51 @@ import { NavLink } from "react-router-dom";
 import Button from "../components/buttons/Button";
 
 const NavBar = () => {
-  return (
-    <div className="container h-[65px] bg-white shadow-sm items-center flex justify-between">
-      <div className="flex items-center gap-4">
-        <img srcSet="src/assets/rekiteku-logo.png 2x" alt="rekiteku-logo" />
-      </div>
-      <div className="flex items-center justify-center gap-16">
-        {navs.map((nav) => (
-          <NavLink
-            key={nav.content}
-            to={nav.to}
-            className={
-              "text-black font-semibold text-[18px] hover:text-primaryColor hover:underline hover:underline-offset-2 transition-all"
-            }
-          >
-            {nav.content}
-          </NavLink>
-        ))}
-      </div>
-      <div>
-        <Button>お問い合わせ</Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container h-[65px] bg-white shadow-sm items-center flex justify-between">
+            <div className="flex items-center gap-4">
+                <img
+                    srcSet="src/assets/rekiteku-logo.png 2x"
+                    alt="rekiteku-logo"
+                />
+            </div>
+            <div className="flex items-center justify-center gap-16">
+                {navs.map((nav) => (
+                    <NavLink
+                        key={nav.content}
+                        to={nav.to}
+                        className={
+                            "text-black font-semibold text-[18px] hover:text-primaryColor hover:underline hover:underline-offset-2 transition-all"
+                        }
+                    >
+                        {nav.content}
+                    </NavLink>
+                ))}
+            </div>
+            <div>
+                <Button>FUCK</Button>
+            </div>
+        </div>
+    );
 };
 
 const navs = [
-  {
-    content: "ホーム",
-    to: "/",
-  },
-  {
-    content: "ノート",
-    to: "/note",
-  },
-  {
-    content: "ツアー",
-    to: "/tour",
-  },
-  {
-    content: "連絡",
-    to: "/contact",
-  },
+    {
+        content: "ホーム",
+        to: "/",
+    },
+    {
+        content: "ノート",
+        to: "/note",
+    },
+    {
+        content: "ツアー",
+        to: "/tour",
+    },
+    {
+        content: "連絡",
+        to: "/contact",
+    },
 ];
 
 export default NavBar;
