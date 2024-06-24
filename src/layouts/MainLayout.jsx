1e3525d18aec915bc2056642b1fd4d5ab5c43508
nam <>
2024-06-24 10:10:36 +0900

    add navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 const MainLayout = () => {
   return (
     <div>
-      <div className='bg-gray-300'>
+      <div className=''>
         <NavBar></NavBar>
       </div>
       <Outlet></Outlet>
