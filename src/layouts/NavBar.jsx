a8236968da777be9f265e4692d929f8ae23866f6
snhrayk <ayaka21sunahara@gmail.com>
2024-06-24 11:42:24 +0900

    test2

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -3,7 +3,6 @@ import { NavLink } from "react-router-dom";
 import Button from "../components/buttons/Button";
 
 const NavBar = () => {
-<<<<<<< HEAD
   return (
     <div className="container h-[65px] bg-white shadow-sm items-center flex justify-between">
       <div className="flex items-center gap-4">
@@ -15,7 +14,7 @@ const NavBar = () => {
             key={nav.content}
             to={nav.to}
             className={
-              "text-pink text-xs font-semibold text-[18px] hover:text-primaryColor hover:underline hover:underline-offset-2 transition-all"
+              "text-black font-semibold text-[18px] hover:text-primaryColor hover:underline hover:underline-offset-2 transition-all"
             }
           >
             {nav.content}
@@ -23,40 +22,11 @@ const NavBar = () => {
         ))}
       </div>
       <div>
-        <Button>FUCK</Button>
+        <Button>お問い合わせ</Button>
       </div>
     </div>
   );
-=======
-    return (
-        <div className="container h-[65px] bg-white shadow-sm items-center flex justify-between">
-            <div className="flex items-center gap-4">
-                <img
-                    srcSet="src/assets/rekiteku-logo.png 2x"
-                    alt="rekiteku-logo"
-                />
-            </div>
-            <div className="flex items-center justify-center gap-16">
-                {navs.map((nav) => (
-                    <NavLink
-                        key={nav.content}
-                        to={nav.to}
-                        className={
-                            "text-black font-semibold text-[18px] hover:text-primaryColor hover:underline hover:underline-offset-2 transition-all"
-                        }
-                    >
-                        {nav.content}
-                    </NavLink>
-                ))}
-            </div>
-            <div>
-                <Button>お問い合わせ</Button>
-            </div>
-        </div>
-    );
->>>>>>> e0f93cdeb79bfc62e8eaea155f38b9285b8388a1
 };
-
 const navs = [
   {
     content: "ホーム",
