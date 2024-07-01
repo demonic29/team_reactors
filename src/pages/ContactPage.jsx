96f1dac31403aad1edd24f07a2ce2b71d74a7198
nam <>
2024-07-01 09:57:32 +0900

    update last folder

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const DivStyles = styled.div`
+	width: 100px;
+	height: 100px;
+	background-color: red;
+	.text {
+		color: yellow;
+		&:hover {
+			background-color: cyan;
+		}
+	}
+`;
 
 const ContactPage = () => {
-  return (
-    <div>
-      Contact Page
-    </div>
-  );
+	return (
+		<DivStyles className="rounded-full">
+			Contact Page
+			<p className="text">namu</p>
+		</DivStyles>
+	);
 };
 
 export default ContactPage;
