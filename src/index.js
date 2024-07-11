8a9ecc9bffa2c88ae96fd0b5cc1b512230ed0205
nam <>
2024-07-11 23:37:12 +0900

    back to component design hash commit

--- src/index.js
+++ src/index.js
@@ -9,33 +9,19 @@ import "react-toastify/dist/ReactToastify.css";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { ModalProvider } from "./contexts/modal-context";
-import CacheBuster from "react-cache-buster";
-import Loading from "components/managerPage/Loading";
-import packageFile from '../package.json';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const isProduction = process.env.NODE_ENV === 'production';
-const { version } = packageFile;
-
 root.render(
-  <CacheBuster
-    currentVersion={version}
-    isEnabled={isProduction} // If false, the library is disabled.
-    isVerboseMode={false} // If true, the library writes verbose logs to console.
-    loadingComponent={<Loading />} // If not pass, nothing appears at the time of new version check.
-    metaFileDirectory={'.'} // If public assets are hosted somewhere other than root on your server.
-  >
-    <React.StrictMode>
-      <ModalProvider>
-        <DndProvider backend={HTML5Backend}>
-          <BrowserRouter>
-            <App /> {/* App Component */}
-            <ToastContainer />
-          </BrowserRouter>
-        </DndProvider>
-      </ModalProvider>
-    </React.StrictMode>
-  </CacheBuster>
+	<React.StrictMode>
+		<ModalProvider>
+			<DndProvider backend={HTML5Backend}>
+				<BrowserRouter>
+					<App /> {/* App Component */}
+					<ToastContainer />
+				</BrowserRouter>
+			</DndProvider>
+		</ModalProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
