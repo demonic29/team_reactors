fc3e47f5cddb2cd4fd7538782b6007d32e5fc1e1
UTakuto <taku.0307.ue@icloud.com>
2024-07-12 10:09:08 +0900

    tour page commit api get display

--- src/pages/TourPage.jsx
+++ src/pages/TourPage.jsx
@@ -8,7 +8,7 @@ import { useApi } from "../contexts/managerPage/api-context";
 const TourPage = () => {
     const mainImg = require("../assets/img/about-main-img.jpg");
     const { data } = useApi();
-    console.log(data);
+    const { tours } = data;
 
     const reviews = [
         {
@@ -56,108 +56,225 @@ const TourPage = () => {
     };
 
     return (
-        <div className="font-kiwiMaru mb-5 ">
-            <div className="container rounded-lg overflow-hidden mt-5">
-                {/* main-img */}
-                <div>
-                    <img
-                        className="w-full aspect-[10/3.5] nd object-cover rounded-lg"
-                        srcSet={`${mainImg}`}
-                        alt="main-building"
-                    />
-                </div>
+        <div>
+            {tours ? (
+                tours.map((tour) => (
+                    <div className="font-kiwiMaru mb-5 ">
+                        <div className="container rounded-lg overflow-hidden mt-5">
+                            {/* main-img */}
+                            <div>
+                                <img
+                                    className="w-full aspect-[10/3.5] nd object-cover rounded-lg"
+                                    srcSet={`${mainImg}`}
+                                    alt="main-building"
+                                />
+                            </div>
 
-                {/* main tour title */}
-                <div className=" mt-[30px] ">
-                    <h1>忠義の福岡武将物語</h1>
-                    <p>福岡の室町から江戸時代の史跡を巡る旅。</p>
-                </div>
-                {/* end tour title */}
+                            {/* main tour title */}
+                            <div className=" mt-[30px] ">
+                                <h1>{tour.title}</h1>
+                                <p>福岡の室町から江戸時代の史跡を巡る旅。</p>
+                            </div>
+                            {/* end tour title */}
 
-                {/* about the fee */}
-                <div className="md:container overflow-hidden ">
-                    <h2 className="mt-[40px]">
-                        <span className="mr-[10px] border-[#00A4EB] border-l-4 "></span>
-                        料金について
-                    </h2>
-                    <div className="mt-[25px] ml-[30px] mb-[50px] ">
-                        <div className="flex  items-center">
-                            <p className=" text-[26px] ">
-                                <span className="mr-[25px] text-[18px] ">
-                                    1泊2日
-                                </span>
-                                ¥20,000
-                            </p>
-                        </div>
-                        <p className="ml-[85px] text-xs text-[#666666] ">
-                            ※食事代、宿泊費、交通費は別途お客様ご本人負担になります。
-                        </p>
-                    </div>
-                </div>
-                {/* end about the fee */}
-
-                {/* Accordion day1 */}
-                <Accordion />
-                {/* end Accordion day1 */}
-
-                {/* Accordion day2 */}
-                <Accordion />
-                {/* end Accordion day2 */}
-
-                {/* gallery */}
-                <div className="max-w-[1000px] m-auto my-[65px] ">
-                    <h2>ギャラリー</h2>
-                    <div className="flex justify-around mt-[30px]">
-                        <div className=" block bg-[#ddd]  w-[700px] min-h-[400px] object-cover rounded-lg"></div>
-
-                        <div className="flex flex-col justify-between">
-                            <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
-                            <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
-                            <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
-                            <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
-                        </div>
-                    </div>
-                </div>
-                {/* end gallery */}
-            </div>
-            {/* review */}
-            <div className="bg-[#333] pt-[60px]">
-                <div
-                    className="max-w-[1000px]  overflow-x-scroll m-auto  bg-cover bg-center "
-                    // style={{
-                    //     backgroundImage: "url('/path/to/background.jpg')",
-                    // }}
-                >
-                    <div className=" min-h-[400px] ">
-                        <div className="flex justify-center">
-                            {reviews.map((review, index) => (
-                                <div
-                                    key={review.id}
-                                    className={`transition-transform duration-300 ease-in-out ${
-                                        centerIndex === index
-                                            ? "transform translate-x-0 scale-110"
-                                            : index < centerIndex
-                                            ? "transform -translate-x-8"
-                                            : "transform translate-x-8"
-                                    }`}
-                                    onClick={() => handleCardClick(index)}
-                                >
-                                    <ReviewCard
-                                        user={review.user}
-                                        review={review.review}
-                                        date={review.date}
-                                        avatar={review.avatar}
-                                    />
+                            {/* about the fee */}
+                            <div className="md:container overflow-hidden ">
+                                <h2 className="mt-[40px]">
+                                    <span className="mr-[10px] border-[#00A4EB] border-l-4 "></span>
+                                    料金について
+                                </h2>
+                                <div className="mt-[25px] ml-[30px] mb-[50px] ">
+                                    <div className="flex  items-center">
+                                        <p className=" text-[26px] ">
+                                            <span className="mr-[25px] text-[18px] ">
+                                                1泊2日
+                                            </span>
+                                            ¥20,000
+                                        </p>
+                                    </div>
+                                    <p className="ml-[85px] text-xs text-[#666666] ">
+                                        ※食事代、宿泊費、交通費は別途お客様ご本人負担になります。
+                                    </p>
                                 </div>
-                            ))}
+                            </div>
+                            {/* end about the fee */}
+
+                            {/* Accordion day1 */}
+                            <Accordion />
+                            {/* end Accordion day1 */}
+
+                            {/* Accordion day2 */}
+                            <Accordion />
+                            {/* end Accordion day2 */}
+
+                            {/* gallery */}
+                            <div className="max-w-[1000px] m-auto my-[65px] ">
+                                <h2>ギャラリー</h2>
+                                <div className="flex justify-around mt-[30px]">
+                                    <div className=" block bg-[#ddd]  w-[700px] min-h-[400px] object-cover rounded-lg"></div>
+
+                                    <div className="flex flex-col justify-between">
+                                        <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
+                                        <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
+                                        <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
+                                        <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* end gallery */}
                         </div>
+                        {/* review */}
+                        <div className="bg-[#333] pt-[60px]">
+                            <div
+                                className="max-w-[1000px]  overflow-x-scroll m-auto  bg-cover bg-center "
+                                // style={{
+                                //     backgroundImage: "url('/path/to/background.jpg')",
+                                // }}
+                            >
+                                <div className=" min-h-[400px] ">
+                                    <div className="flex justify-center">
+                                        {reviews.map((review, index) => (
+                                            <div
+                                                key={review.id}
+                                                className={`transition-transform duration-300 ease-in-out ${
+                                                    centerIndex === index
+                                                        ? "transform translate-x-0 scale-110"
+                                                        : index < centerIndex
+                                                        ? "transform -translate-x-8"
+                                                        : "transform translate-x-8"
+                                                }`}
+                                                onClick={() =>
+                                                    handleCardClick(index)
+                                                }
+                                            >
+                                                <ReviewCard
+                                                    user={review.user}
+                                                    review={review.review}
+                                                    date={review.date}
+                                                    avatar={review.avatar}
+                                                />
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/*　end review */}
+                        {/*　footer */}
                     </div>
+                ))
+            ) : (
+                <div className=" flex justify-center items-center h-screen text-[20px] ">
+                    loading
                 </div>
-            </div>
-            {/*　end review */}
-            {/*　footer */}
+            )}
         </div>
     );
 };
 
 export default TourPage;
+// <div className="font-kiwiMaru mb-5 ">
+//     <div className="container rounded-lg overflow-hidden mt-5">
+//         {/* main-img */}
+//         <div>
+//             <img
+//                 className="w-full aspect-[10/3.5] nd object-cover rounded-lg"
+//                 srcSet={`${mainImg}`}
+//                 alt="main-building"
+//             />
+//         </div>
+
+//         {/* main tour title */}
+//         <div className=" mt-[30px] ">
+//             <h1>
+//                 福岡が古来から海外と密接に関係した国内唯一の地
+//             </h1>
+//             <p>福岡の室町から江戸時代の史跡を巡る旅。</p>
+//         </div>
+//         {/* end tour title */}
+
+//         {/* about the fee */}
+//         <div className="md:container overflow-hidden ">
+//             <h2 className="mt-[40px]">
+//                 <span className="mr-[10px] border-[#00A4EB] border-l-4 "></span>
+//                 料金について
+//             </h2>
+//             <div className="mt-[25px] ml-[30px] mb-[50px] ">
+//                 <div className="flex  items-center">
+//                     <p className=" text-[26px] ">
+//                         <span className="mr-[25px] text-[18px] ">
+//                             1泊2日
+//                         </span>
+//                         ¥20,000
+//                     </p>
+//                 </div>
+//                 <p className="ml-[85px] text-xs text-[#666666] ">
+//                     ※食事代、宿泊費、交通費は別途お客様ご本人負担になります。
+//                 </p>
+//             </div>
+//         </div>
+//         {/* end about the fee */}
+
+//         {/* Accordion day1 */}
+//         <Accordion />
+//         {/* end Accordion day1 */}
+
+//         {/* Accordion day2 */}
+//         <Accordion />
+//         {/* end Accordion day2 */}
+
+//         {/* gallery */}
+//         <div className="max-w-[1000px] m-auto my-[65px] ">
+//             <h2>ギャラリー</h2>
+//             <div className="flex justify-around mt-[30px]">
+//                 <div className=" block bg-[#ddd]  w-[700px] min-h-[400px] object-cover rounded-lg"></div>
+
+//                 <div className="flex flex-col justify-between">
+//                     <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
+//                     <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
+//                     <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
+//                     <div className=" block bg-[#ddd]  w-[240px] min-h-[85px] object-cover rounded-lg"></div>
+//                 </div>
+//             </div>
+//         </div>
+//         {/* end gallery */}
+//     </div>
+//     {/* review */}
+//     <div className="bg-[#333] pt-[60px]">
+//         <div
+//             className="max-w-[1000px]  overflow-x-scroll m-auto  bg-cover bg-center "
+//             // style={{
+//             //     backgroundImage: "url('/path/to/background.jpg')",
+//             // }}
+//         >
+//             <div className=" min-h-[400px] ">
+//                 <div className="flex justify-center">
+//                     {reviews.map((review, index) => (
+//                         <div
+//                             key={review.id}
+//                             className={`transition-transform duration-300 ease-in-out ${
+//                                 centerIndex === index
+//                                     ? "transform translate-x-0 scale-110"
+//                                     : index < centerIndex
+//                                     ? "transform -translate-x-8"
+//                                     : "transform translate-x-8"
+//                             }`}
+//                             onClick={() =>
+//                                 handleCardClick(index)
+//                             }
+//                         >
+//                             <ReviewCard
+//                                 user={review.user}
+//                                 review={review.review}
+//                                 date={review.date}
+//                                 avatar={review.avatar}
+//                             />
+//                         </div>
+//                     ))}
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     {/*　end review */}
+//     {/*　footer */}
+// </div>
