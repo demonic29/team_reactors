078dfa9577a8d159d72430d4a384b9f16958d4bd
nam <>
2024-07-20 17:29:35 +0900

    fix homeAbout UI

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -9,137 +9,142 @@ import note from "../assets/img/Note Logo.svg";
 import { NavLink } from "react-router-dom";
 
 export default function Footer() {
-    const { loading, data } = useApi();
-    const [footers, setFooters] = useState([]);
-    useEffect(() => {
-        if (data && data.footer) {
-            setFooters(data.footer);
-        }
-    }, [data]);
+	const { loading, data } = useApi();
+	const [footers, setFooters] = useState([]);
+	useEffect(() => {
+		if (data && data.footer) {
+			setFooters(data.footer);
+		}
+	}, [data]);
 
-    return (
-        <>
-            {loading ? (
-                <div className="skeleton"></div>
-            ) : (
-                <>
-                    {footers && (
-                        <footer className=" bg-secondaryColor mt-[100px] pt-[50px] px-[200px] pb-[16px]">
-                            {/* ロゴ・SNS・連絡先のWrap */}
-                            <div className="container flex justify-evenly gap-x-16">
-                                {/* 左半分 ロゴとSNS */}
-                                <div className="flex flex-col gap-y-6">
-                                    {/* ロゴと企業名 */}
-                                    <div className="flex items-center gap-x-6">
-                                        <img
-                                            src={`${logo}`}
-                                            alt="歴てくのロゴ"
-                                        />
-                                        <p className="text-2xl font-bold text-white">
-                                            {footers.companyName}
-                                        </p>
-                                        {/* <p className="text-4xl font-bold text-white">
+	return (
+		<>
+			{loading ? (
+				<div className="skeleton"></div>
+			) : (
+				<>
+					{footers && (
+						<footer className=" bg-secondaryColor mt-[100px] pt-[50px] pb-[16px]">
+							{/* ロゴ・SNS・連絡先のWrap */}
+							<div className="container flex justify-between gap-x-16">
+								{/* 左半分 ロゴとSNS */}
+								<div className="flex flex-col gap-y-6">
+									{/* ロゴと企業名 */}
+									<div className="flex items-center gap-x-6">
+										<img
+											src={`${logo}`}
+											alt="歴てくのロゴ"
+										/>
+										<div>
+											<p className="mb-2 text-[28px] font-bold text-white">
+												{footers.companyName}
+											</p>
+											<p className="text-lg font-bold text-white opacity-70">
+												{`${footers.slogan}`}
+											</p>
+										</div>
+										{/* <p className="text-4xl font-bold text-white">
                                             歴てく
                                             <span className="text-2xl">
                                                 -INSIDE-
                                             </span>
                                         </p> */}
-                                    </div>
-                                    {/* SNS */}
-                                    <div>
-                                        {footers.sns && (
-                                            <ul className="flex gap-x-6 items-center">
-                                                <li>
-                                                    <a href={footers.sns.fb}>
-                                                        <img
-                                                            src={fb}
-                                                            alt="facebook"
-                                                        />
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href={footers.sns.insta}>
-                                                        <img
-                                                            src={insta}
-                                                            alt="instagram"
-                                                        />
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href={footers.sns.x}>
-                                                        <img src={x} alt="x" />
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href={footers.sns.note}>
-                                                        <img
-                                                            src={note}
-                                                            alt="note"
-                                                        />
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        )}
-                                    </div>
-                                </div>
-                                {/* 右半分 連絡先 */}
-                                <div className="flex flex-col gap-y-8">
-                                    <div>
-                                        <span className="text-slate-300 text-lg">
-                                            メール
-                                        </span>
-                                        <p className="text-white text-base">
-                                            {footers.email}
-                                        </p>
-                                    </div>
-                                    <div className="flex gap-x-16">
-                                        <div>
-                                            <span className="text-slate-300 text-lg">
-                                                電話番号
-                                            </span>
-                                            <p className="text-white text-base">
-                                                {footers.phone}
-                                            </p>
-                                        </div>
-                                        <div>
-                                            <span className="text-slate-300 text-lg">
-                                                住所
-                                            </span>
-                                            <p className="text-white text-base">
-                                                {footers.address}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* プライバシポリシー系 */}
-                            <div className="my-16">
-                                <ul className="flex justify-center gap-x-20 text-white text-xs font-thin">
-                                    <li>
-                                        <NavLink to={"/travelAgencyAgreement"}>
-                                            旅行業約款
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to={"/PrivacyPolicyPage"}>
-                                            プライバシーポリシー
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        国内旅行業務取扱管理者第〇〇〇〇-〇〇〇〇号
-                                    </li>
-                                </ul>
-                            </div>
-                            {/* copylight */}
-                            <p className="text-center text-xs">
-                                <small className="text-slate-300">
-                                    {footers.copyright}
-                                </small>
-                            </p>
-                        </footer>
-                    )}
-                </>
-            )}
-        </>
-    );
+									</div>
+									{/* SNS */}
+									<div>
+										{footers.sns && (
+											<ul className="flex items-center gap-x-6">
+												<li>
+													<a href={footers.sns.fb}>
+														<img
+															src={fb}
+															alt="facebook"
+														/>
+													</a>
+												</li>
+												<li>
+													<a href={footers.sns.insta}>
+														<img
+															src={insta}
+															alt="instagram"
+														/>
+													</a>
+												</li>
+												<li>
+													<a href={footers.sns.x}>
+														<img src={x} alt="x" />
+													</a>
+												</li>
+												<li>
+													<a href={footers.sns.note}>
+														<img
+															src={note}
+															alt="note"
+														/>
+													</a>
+												</li>
+											</ul>
+										)}
+									</div>
+								</div>
+								{/* 右半分 連絡先 */}
+								<div className="flex flex-col gap-y-8">
+									<div>
+										<span className="text-lg text-slate-300">
+											メール
+										</span>
+										<p className="text-base text-white">
+											{footers.email}
+										</p>
+									</div>
+									<div className="flex gap-x-16">
+										<div>
+											<span className="text-lg text-slate-300">
+												電話番号
+											</span>
+											<p className="text-lg text-white ">
+												{footers.phone}
+											</p>
+										</div>
+										<div>
+											<span className="text-lg text-slate-300">
+												住所
+											</span>
+											<p className="text-lg text-white ">
+												{footers.address}
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							{/* プライバシポリシー系 */}
+							<div className="my-8">
+								<ul className="flex justify-center text-base font-thin text-white gap-x-20">
+									<li>
+										<NavLink to={"/travelAgencyAgreement"}>
+											旅行業約款
+										</NavLink>
+									</li>
+									<li>
+										<NavLink to={"/PrivacyPolicyPage"}>
+											プライバシーポリシー
+										</NavLink>
+									</li>
+									<li>
+										国内旅行業務取扱管理者第〇〇〇〇-〇〇〇〇号
+									</li>
+								</ul>
+							</div>
+							{/* copylight */}
+							<p className="text-sm text-center">
+								<small className="text-slate-300">
+									{footers.copyright}
+								</small>
+							</p>
+						</footer>
+					)}
+				</>
+			)}
+		</>
+	);
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,121 +2,131 @@ import React, { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "firebase-config";
 import Footer from "layouts/Footer";
-import { logDOM } from "@testing-library/react";
 
 export const fetchAboutData = async () => {
-  const docRef = doc(db, "general", "pageData");
-  const docSnap = await getDoc(docRef);
+	const docRef = doc(db, "general", "pageData");
+	const docSnap = await getDoc(docRef);
 
-  if (docSnap.exists()) {
-    return docSnap.data();
-  } else {
-    console.log("No such document!");
-    return null;
-  }
+	if (docSnap.exists()) {
+		return docSnap.data();
+	} else {
+		console.log("No such document!");
+		return null;
+	}
 };
 
 const AboutPage = () => {
-  const [pageData, setPageData] = useState(null);
+	const [pageData, setPageData] = useState(null);
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const data = await fetchAboutData();
-        setPageData(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const data = await fetchAboutData();
+				setPageData(data);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
 
-    getData();
-  }, []);
+		getData();
+	}, []);
 
-  if (!pageData) {
-    return <div>Loading...</div>;
-  }
+	if (!pageData) {
+		return <div>Loading...</div>;
+	}
 
-  console.log(pageData);
+	console.log(pageData);
 
-  const accessLink = () => {
-    window.open(
-      pageData.about.owner.accessLink,
-      "_blank",
-      "noopener,noreferrer"
-    );
-  };
+	const accessLink = () => {
+		window.open(
+			pageData.about.owner.accessLink,
+			"_blank",
+			"noopener,noreferrer"
+		);
+	};
 
-  return (
-    <div>
-      {/* main-img */}
-      <div className="container rounded-lg overflow-hidden mt-5">
-        <img
-          className="w-full aspect-[10/3.5] nd object-cover rounded-lg"
-          src={pageData.about.vision.image.downloadURL}
-          alt={pageData.about.vision.image.name}
-        />
-      </div>
+	const aboutData = pageData.about || {};
 
-      {/* main-text */}
-      <div className="text-center mt-[50px] flex justify-center">
-        <div className="w-full max-w-[1080px] px-10">
-          <h2 className="text-3xl font-bold mb-10">我々の理念</h2>
-          <p className="text-gray-800 text-xl leading-8">
-            {pageData.about.vision.content}
-          </p>
-        </div>
-      </div>
+	return (
+		<div>
+			{/* main-img */}
+			<div className="container mt-5 overflow-hidden rounded-lg">
+				<img
+					className="w-full aspect-[10/3.5] nd object-cover rounded-lg"
+					src={aboutData.banner.downloadURL}
+					alt="about-page-banner"
+				/>
+			</div>
 
-      {/* main-profile */}
-      <div className="grid grid-cols-2 mt-[150px] bg-primaryColor">
-        <div className="m-auto pl-5">
-          <h2 className="text-[50px] font-bold mb-5 text-white">
-            {pageData.about.owner.name}
-          </h2>
-          <p className="text-slate-100 text-xl leading-7 tracking-wider">
-            {pageData.about.owner.introduce}
-          </p>
-          <div className="mt-[80px]">
-            <button
-              onClick={accessLink}
-              className="text-black bg-white px-8 py-4 rounded-md transition-all hover:bg-slate-200"
-            >
-              {pageData.about.owner.buttonContent}
-            </button>
-          </div>
-        </div>
-        <div>
-          <img
-            className="w-[100%] h-[600px] object-cover"
-            src={pageData.about.owner.image.downloadURL}
-            alt={pageData.about.owner.image.name}
-          />
-        </div>
-      </div>
+			{/* main-text */}
+			<div className="text-center mt-[50px] flex justify-center">
+				<div className="w-full max-w-[1080px] px-10">
+					<h2 className="mb-10 text-3xl font-bold">我々の理念</h2>
+					<p className="text-xl leading-8 text-gray-800">
+						{aboutData.vision.content}
+					</p>
+				</div>
+			</div>
 
-      {/* company-infos */}
-      <h2 className="text-3xl text-center font-bold mt-[100px]">会社概要</h2>
-      <div className="flex mt-[50px] justify-evenly items-center">
-        <div>
-          <ul className="grid gap-5">
-            <li>会社名（事業名） : {pageData.about.access.companyData.name}</li>
-            <li>成立 : {pageData.about.access.companyData.foundDate}</li>
-            <li>住所 : {pageData.about.access.companyData.access}</li>
-            <li className="mt-8">
-              連絡先 : {pageData.about.access.contact.phoneNumber}
-            </li>
-            <li>問い合わせメール : {pageData.about.access.contact.email}</li>
-          </ul>
-        </div>
-        <div
-          className="rounded-lg"
-          dangerouslySetInnerHTML={{ __html: pageData.about.access.map }}
-        />
-      </div>
+			{/* main-profile */}
+			<div className="grid grid-cols-2 mt-[150px] bg-primaryColor">
+				<div className="pl-5 my-auto w-full max-w-[620px] ml-auto pr-16 leading-relaxed">
+					<h2 className="text-[50px] font-bold mb-5 text-white">
+						{aboutData.owner.name}
+					</h2>
+					<p className="text-xl leading-7 tracking-wider text-slate-100">
+						{aboutData.owner.introduce}
+					</p>
+					<div className="mt-[50px]">
+						<button
+							onClick={accessLink}
+							className="px-8 py-4 text-black transition-all bg-white rounded-md hover:bg-slate-200"
+						>
+							{aboutData.owner.buttonContent}
+						</button>
+					</div>
+				</div>
+				<div>
+					<img
+						className="w-[100%] h-[600px] object-cover"
+						src={aboutData.owner.image.downloadURL}
+						alt={aboutData.owner.image.name}
+					/>
+				</div>
+			</div>
 
-      <Footer />
-    </div>
-  );
+			{/* company-infos */}
+			<h2 className="text-3xl text-center font-bold mt-[100px]">
+				会社概要
+			</h2>
+			<div className="container flex text-lg mt-[50px] justify-between items-center">
+				<div>
+					<ul className="grid gap-5">
+						<li>
+							会社名（事業名） :{" "}
+							{aboutData.access.companyData.name}
+						</li>
+						<li>成立 : {aboutData.access.companyData.foundDate}</li>
+						<li>住所 : {aboutData.access.companyData.access}</li>
+						<li className="mt-8">
+							連絡先 : {aboutData.access.contact.phoneNumber}
+						</li>
+						<li>
+							問い合わせメール : {aboutData.access.contact.email}
+						</li>
+					</ul>
+				</div>
+				<div
+					className="scale-75 rounded-lg"
+					dangerouslySetInnerHTML={{
+						__html: aboutData.access.map,
+					}}
+				></div>
+			</div>
+
+			<Footer />
+		</div>
+	);
 };
 
 export default AboutPage;
