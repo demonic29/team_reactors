4c78a6af6dbfbe449bd7b78db8e28dc244f5909e
UTakuto <taku.0307.ue@icloud.com>
2024-07-26 17:11:49 +0900

    accordion fixes

--- src/layouts/Accordion.jsx
+++ src/layouts/Accordion.jsx
@@ -2,12 +2,14 @@ import { useApi } from "../contexts/managerPage/api-context";
 import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import { BsQuestionCircle } from "react-icons/bs";
 
 export default function Accordion() {
     const { data } = useApi();
 
     const [tourList, setTourList] = useState([]);
     const [activeIds, setActiveIds] = useState([]);
+    const [isHovered, setIsHovered] = useState(false);
 
     useEffect(() => {
         if (data && data.tours && data.tours.length > 0) {
@@ -83,6 +85,7 @@ export default function Accordion() {
                                 />
                             </svg>
                         </button>
+
                         <div
                             className={`overflow-hidden transition-all duration-700 ease-in-out ${
                                 activeIds.includes(plan.id)
@@ -93,20 +96,54 @@ export default function Accordion() {
                                 transitionProperty: "max-height",
                             }}
                         >
-                            <div className="flex justify-between items-start">
+                            <div className="flex items-start justify-between">
                                 <div className="min-h-[330px] flex flex-col justify-between">
-                                    <p>{plan.start}</p>
-                                    {plan.destination &&
-                                        plan.destination.length > 0 &&
-                                        plan.destination.map((destination) => (
-                                            <p
-                                                key={destination.id}
-                                                className="before:block before:w-[15px] before:h-[15px] before:bg-[#0075D4] before:rounded-[50vh] before:mr-[20px] flex items-center"
-                                            >
-                                                {destination.name}
-                                            </p>
-                                        ))}
-                                    <p>{plan.end}</p>
+                                    <p>集合 : {plan.start}</p>
+                                    <div className="flex justify-start items-start">
+                                        <ul className="timeline timeline-vertical flex  ">
+                                            {plan.destination &&
+                                                plan.destination.length > 0 &&
+                                                plan.destination.map(
+                                                    (destination, index) => (
+                                                        <li
+                                                            key={destination.id}
+                                                        >
+                                                            {index !== 0 && (
+                                                                <hr />
+                                                            )}
+                                                            <div className="timeline-start p-[20px]">
+                                                                {destination.id}
+                                                            </div>
+                                                            <div className="timeline-middle">
+                                                                <svg
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                    viewBox="0 0 20 20"
+                                                                    fill="currentColor"
+                                                                    className="h-5 w-5"
+                                                                >
+                                                                    <path
+                                                                        fillRule="evenodd"
+                                                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75"
+                                                                        clipRule="evenodd"
+                                                                    />
+                                                                </svg>
+                                                            </div>
+                                                            <div className="timeline-end timeline-box">
+                                                                {
+                                                                    destination.name
+                                                                }
+                                                            </div>
+                                                            {index !==
+                                                                plan.destination
+                                                                    .length -
+                                                                    1 && <hr />}
+                                                        </li>
+                                                    )
+                                                )}
+                                        </ul>
+                                    </div>
+
+                                    <p>解散 : {plan.end}</p>
                                 </div>
                                 <div>
                                     {plan.map && (
@@ -128,7 +165,36 @@ export default function Accordion() {
                             </div>
 
                             <div className="mt-[30px] flex flex-col items-center">
-                                <h2 className="my-[50px]">体力面について</h2>
+                                <h2 className="my-[50px] flex justify-center items-center">
+                                    体力面について
+                                    <span className="pl-[10px] pt-[5px] relative">
+                                        <BsQuestionCircle
+                                            color="#999"
+                                            size="18px"
+                                            onMouseEnter={() =>
+                                                setIsHovered(true)
+                                            }
+                                            onMouseLeave={() =>
+                                                setIsHovered(false)
+                                            }
+                                        />
+                                        <div
+                                            className={`absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-[500px] bg-[#004B88] text-[#fefefe] text-[12px] rounded-lg py-2 px-4 transition-opacity duration-500 ease-in-out 
+                                                ${
+                                                    isHovered
+                                                        ? "opacity-100 scale-95"
+                                                        : "opacity-0 scale-95"
+                                                }`}
+                                        >
+                                            <p>※体力消費率について :</p>
+                                            {/* <div className="max-w-[80%] m-auto "> */}
+                                            <p className=" pl-[12px] ">
+                                                各地点を訪れる際にどれくらい体力を消費するかを、5段階で評価し可視化したものです。星の数が増えるほど、体力の消費が大きくなることを示しています。
+                                            </p>
+                                            {/* </div> */}
+                                        </div>
+                                    </span>
+                                </h2>
                                 <div className="flex flex-wrap justify-between items-center ">
                                     {tourList.locations &&
                                         tourList.locations.length > 0 &&
@@ -200,12 +266,6 @@ export default function Accordion() {
                                                     </div>
                                                 </div>
                                             ))}
-                                </div>
-                                <div className="flex justify-center mx-auto align-center text-[12px] text-[#9f9f9f] pb-[30px] ">
-                                    <p>※体力消費率について :</p>
-                                    <p className="max-w-[50%]">
-                                        各地点を訪れる際にどれくらい体力を消費するかを、5段階で評価し可視化したものです。星の数が増えるほど、体力の消費が大きくなることを示しています。
-                                    </p>
                                 </div>
                             </div>
                         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/**/*.{js,jsx,ts,tsx}",
-	],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
 
-	theme: {
-		extend: {
-			colors: {
-				primaryColor: "#0078DA",
-				secondaryColor: "#004B88",
-			},
-		},
-		fontFamily: {
-      kiwiMaru: ["Kiwi Maru", "serif"],
+    theme: {
+        extend: {
+            colors: {
+                primaryColor: "#0078DA",
+                secondaryColor: "#004B88",
+            },
+        },
+        fontFamily: {
+            kiwiMaru: ["Kiwi Maru", "serif"],
+        },
+        screens: {
+            "2xl": { max: "1535px" },
+            // => @media (max-width: 1535px) { ... }
+
+            xl: { max: "1279px" },
+            // => @media (max-width: 1279px) { ... }
+
+            lg: { max: "1023px" },
+            // => @media (max-width: 1023px) { ... }
+
+            md: { max: "767px" },
+            // => @media (max-width: 767px) { ... }
+
+            sm: { max: "639px" },
+            // => @media (max-width: 639px) { ... }
+        },
     },
-		screens: {
-			"2xl": { max: "1535px" },
-			// => @media (max-width: 1535px) { ... }
 
-			xl: { max: "1279px" },
-			// => @media (max-width: 1279px) { ... }
-
-			lg: { max: "1023px" },
-			// => @media (max-width: 1023px) { ... }
-
-			md: { max: "767px" },
-			// => @media (max-width: 767px) { ... }
-
-			sm: { max: "639px" },
-			// => @media (max-width: 639px) { ... }
-		},
-	},
-
-	plugins: [
-	],
+    plugins: [require("daisyui")],
 };
